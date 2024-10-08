bb35f3385ada77c86bbe5d0e3fe57e5c17552190
revert12 <97959984+revert12@users.noreply.github.com>
2024-10-08 13:40:36 +0900

    Update nvidia.sh

--- nvidia.sh
+++ nvidia.sh
@@ -27,6 +27,7 @@ if [[ $EUID -ne 0 ]]; then
 echo "이 스크립트를 실행하려면 root 권한이 필요합니다." 
 exit 1
 fi
+
 # 파일이 존재하는지 확인
 if [[ -f "$AUTO_UPGRADE_FILE" ]]; then
 # 자동 업데이트 비활성화
@@ -38,8 +39,17 @@ echo "자동 업데이트가 비활성화되었습니다."
 else
 echo "파일 '$AUTO_UPGRADE_FILE'가 존재하지 않습니다."
 fi
+
+CUDA_VERSION=$(nvcc -V 2>/dev/null)
+
+if [ $? -eq 0 ]; then
+echo "CUDA가 정상적으로 설치되었습니다."
+echo "$CUDA_VERSION"
 # 설치 파일 삭제
 rm -f cuda_11.7.0_515.43.04_linux.run || { echo "Failed to delete CUDA installer"; exit 1; }
 echo "CUDA 설치 파일이 삭제되었습니다."
+else
+echo "설치 파일을 찾을 수 없습니다"
+fi
 
 echo "NVIDIA 드라이버 및 CUDA 11.7 설치 완료, 환경 변수 설정 완료."
