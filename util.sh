0dd654a684098a98e5a5a49d1f26531bf6be84c6
revert12 <97959984+revert12@users.noreply.github.com>
2024-10-28 16:18:02 +0900

    Update util.sh

--- util.sh
+++ util.sh
@@ -8,41 +8,65 @@ log_error() {
 sudo apt-get update
 
 # bpytop 설치
-if ! command -v bpytop &> /dev/null; then
+if  command -v bpytop &> /dev/null; then
+    echo "bpytop is already installed."
+
+else
     echo "Installing bpytop..."
     sudo snap install -y bpytop
-else
-    echo "bpytop is already installed."
 fi
 
 # vim 설치
 if command -v vim &> /dev/null; then
-        echo "Vim is already installed."
+    echo "Vim is already installed."
+    
+        if grep -q "set number" ~/.vimrc; then
+            echo "'set number' is already present in ~/.vimrc."
+        else
+            echo "set number" >> ~/.vimrc
+            echo "'set number' added to ~/.vimrc."
+        fi       
 else
         if sudo apt-get install -y vim; then
             echo "Vim installed successfully."
+            echo "set number" >> ~/.vimrc
+            echo "'set number' added to ~/.vimrc."
         else
             log_error "Failed to install Vim."
-        fi
-fi
-
-# Add 'set number' to .vimrc
-if ! grep -q "set number" ~/.vimrc; then
-    echo "set number" >> ~/.vimrc
-    echo "'set number' added to ~/.vimrc."
-else
-    echo "'set number' is already present in ~/.vimrc."
+        fi       
 fi
 
 # net-tools install
 if command -v ifconfig &> /dev/null; then
-        echo "net-tools is already installed."
+        echo "net-tools is already installed."       
 else 
-    if sudo apt-get install -y net-tools; then
-        echo "net-tools installed successfully."
-    else
-        log_error "Failed to install net-tools."
-    fi
+        if sudo apt-get install -y net-tools; then
+            echo "net-tools installed successfully."
+        else
+            log_error "Failed to install net-tools."
+        fi 
+fi
+
+# vlc install
+if command -v vlc &> /dev/null; then
+        echo "vlc is already installed."
+else 
+        if sudo apt-get install -y vlc; then
+            echo "vlc installed successfully."
+        else
+            log_error "Failed to install vlc."
+        fi 
+fi
+
+# anydesk install
+if command -v anydesk &> /dev/null; then
+        echo "anydesk is already installed."
+else 
+        if sudo apt-get install -y anydesk; then
+            echo "anydesk installed successfully."
+        else
+            log_error "Failed to install anydesk."
+        fi 
 fi
 
 
