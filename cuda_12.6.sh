a147016137c0d880aebcade6d230b9838d892907
revert12 <97959984+revert12@users.noreply.github.com>
2024-11-25 14:19:01 +0900

    Update cuda_12.6.sh

--- cuda_12.6.sh
+++ cuda_12.6.sh
@@ -60,7 +60,7 @@ install_cuda() {
         echo "CUDA is not installed. Starting installation."
         wget https://developer.download.nvidia.com/compute/cuda/12.6.3/local_installers/cuda_12.6.3_560.35.05_linux.run || log_error "Failed to download CUDA installer"
         chmod +x cuda_12.6.3_560.35.05_linux.run || log_error "Failed to make CUDA installer executable"
-        sudo sh cuda_12.6.3_560.35.05_linux.run || log_error "Failed to install CUDA"
+        sudo cuda_12.6.3_560.35.05_linux.run || log_error "Failed to install CUDA"
     fi
 
     # .bashrc에 설정이 이미 존재하는지 확인
